2e002520359dd09b34f4ea494e4d1ac284c8c60b
John Matos <rDr4g0n@gmail.com>
2017-07-26 18:55:09 -0500

    table render functions more good

--- src/CRUDTable.js
+++ src/CRUDTable.js
@@ -8,23 +8,26 @@ function renderTableRow(actions, columns, model){
     let keys = this.columns.slice(0)
     keys.push("actions")
     
-    let html = []
-    keys.forEach(key => {
-        if(key === "actions"){
-            html.push(`<td class="crud-table-row-cell crud-table-row-actions-cell">
-                ${ actions.map(action => `<i class="action fa fa-fw fa-${ACTION_ICONS[action]}" data-action="${action}" data-id="${model.id}" title="${action}"></i>`).join("") }
-            </td>`)
-        } else {
-            html.push(`<td class="crud-table-row-cell">${model[key]}</td>`)
-        }
-    })
-    return html.join("")
+    return model.map(rowItem => {
+        let html = [`<tr class="crud-table-row">`]
+        keys.forEach(key => {
+            if(key === "actions"){
+                html.push(`<td class="crud-table-row-cell crud-table-row-actions-cell">
+                    ${ actions.map(action => `<i class="action fa fa-fw fa-${ACTION_ICONS[action]}" data-action="${action}" data-id="${rowItem.id}" title="${action}"></i>`).join("") }
+                </td>`)
+            } else {
+                html.push(`<td class="crud-table-row-cell">${rowItem[key]}</td>`)
+            }
+        })
+        html.push("</tr>")
+        return html.join("")
+    }).join("")
 }
 function renderTableHeader(actions, columns, model){
     let keys = this.columns.slice(0)
     keys.push("actions")
     
-    let html = []
+    let html = ["<tr class='crud-table-row'>"]
     keys.forEach(key => {
         if(key === "actions"){
             html.push(`<th class="crud-table-row-header-cell crud-table-row-cell crud-table-row-actions-cell">${key}</th>`)
@@ -32,60 +35,11 @@ function renderTableHeader(actions, columns, model){
             html.push(`<th class="crud-table-row-header-cell crud-table-row-cell">${key}</th>`)
         }
     })
+    html.push("</tr>")
     return html.join("")
 }
 function renderTableFooter(actions, columns, model){
-    let keys = this.columns.slice(0)
-    keys.push("actions")
-    let html = []
-    html.push(`<td class="crud-table-footer-row-cell" colspan="${columns.length + 1}">Showing ${model.length} results</td>`)
-    return html.join("")
-}
-
-class CRUDTableRow {
-    constructor(config){
-        this.model = config.model
-        this.header = config.header
-        this.actions = config.actions || []
-        this.columns = config.columns
-
-        this.el = document.createElement("tr")
-        this.el.className = "crud-table-row"
-
-        if(this.header){
-            this.el.classList.add("crud-table-row-header")
-        }
-
-        this.render()
-    }
-
-    render(){
-        let html = []
-
-        let keys = this.columns.slice(0)
-
-        keys.push("actions")
-
-        keys.forEach(key => {
-            if(this.header){
-                if(key === "actions"){
-                    html.push(`<th class="crud-table-row-header-cell crud-table-row-cell crud-table-row-actions-cell">${key}</th>`)
-                } else {
-                    html.push(`<th class="crud-table-row-header-cell crud-table-row-cell">${key}</th>`)
-                }
-            } else {
-                if(key === "actions"){
-                    html.push(`<td class="crud-table-row-cell crud-table-row-actions-cell">
-                        ${ this.actions.map(action => `<i class="action fa fa-fw fa-${ACTION_ICONS[action]}" data-action="${action}" data-id="${this.model.id}" title="${action}"></i>`).join("") }
-                    </td>`)
-                } else {
-                    html.push(`<td class="crud-table-row-cell">${this.model[key]}</td>`)
-                }
-            }
-        })
-
-        this.el.innerHTML = html.join("")
-    }
+    return `<tr><td class="crud-table-footer-row-cell" colspan="${columns.length + 1}">Showing ${model.length} results</td></tr>`
 }
 
 export default class CRUDTable {
@@ -94,6 +48,8 @@ export default class CRUDTable {
         this.apiEndpoint = config.apiEndpoint
         this.actions = config.actions
         this.columns = []
+
+        this.api = config.api
 
         // TODO - opionally get config from user
         this.headerRenderer = renderTableHeader
@@ -106,7 +62,6 @@ export default class CRUDTable {
         // TODO - pagination
         // TODO - selct and apply action to multiple rows
         // TODO - separate api adapter into its own lil interface
-        // TODO - use a template string to setup the initial html
         // TODO - add concept of schema (field label, field editable, field type, validation rules, 
         // relationships (id looksup a different object), etc). initally generate a dumb schema
         // from input, but later let user define
@@ -153,39 +108,26 @@ export default class CRUDTable {
         if(confirm("do you want to create a random restaurant instead of using a nice form to enter the actual info?")){
             let model = {}
             this.columns.forEach(col => model[col] = Math.random())
-            let result = await this.doCreate(model)
-            // TODO - refresh
+            let result = await this.create(model)
+            await this.update()
         }
     }
-    async doCreate(model){
-        try {
-            let form = new FormData()
-            for(let key in model){
-                form.append(key, model[key])
-            }
-            let response = await fetch(`${this.apiEndpoint}/${this.entity}`, { 
-                method: "POST",
-                body: form
+    create(model){
+        return this.api.create(model)
+            .then(response => console.log("create success i guess", response))
+            .catch(e => console.error("oops", e))
+    }
+
+    update(){
+        return this.api.retrieve()
+            .then(data => {
+                this.columns = Object.keys(data[0])
+                this.render(data)
             })
-        } catch(err) {
-            console.error("oops", err)
-        }
+            .catch(e => console.error("oops", e))
     }
 
-    async update(){
-        try {
-            let response = await fetch(`${this.apiEndpoint}/${this.entity}`)
-            let data = await response.json()
-            // TODO - i guess dont assume data[0] exists and represents 
-            // all the objects in the results
-            this.columns = Object.keys(data[0])
-            this.renderRows(data)
-        } catch(err) {
-            console.error("oops", err)
-        }
-    }
-
-    renderRows(model){
+    render(model){
         this.table.innerHTML = `
             ${this.headerRenderer(this.actions.row, this.columns, model)}
             ${this.rowRenderer(this.actions.row, this.columns, model)}
--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import CRUDTable from "./CRUDTable"
 
+let url = "http://65.111.114.167/waitme/index.php/restaurant"
+// adyard api adapter
+let api = {
+    create(data){
+        let form = new FormData()
+        for(let key in data){
+            form.append(key, data[key])
+        }
+        return fetch(url, { 
+            method: "POST",
+            body: form
+        })
+    },
+    // TODO - search, filter, sort, pagination
+    retrieve(){
+        return fetch(url).then(data => data.json())
+    }
+}
+
 let table = new CRUDTable({
     apiEndpoint: "http://65.111.114.167/waitme/index.php",
+    api: api,
     entity: "restaurant",
     actions: {
         table: ["create"],
